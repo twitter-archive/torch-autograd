03f3c0de27d2f519e8de29403a40f4e46996b340
Clement Farabet <cfarabet@twitter.com>
2015-09-21 12:09:34 -0400

    More tests.

--- test.lua
+++ test.lua
@@ -6,6 +6,33 @@ local tester = totem.Tester()
 
 -- List of tests:
 local tests = {
+   Dot = function()
+      -- Parameters:
+      local W = torch.Tensor(32,100):fill(.5)
+      local x = torch.Tensor(100):fill(.5)
+
+      -- Function:
+      local func = function(inputs)
+         return torch.sum(inputs.W * inputs.x)
+      end
+
+      -- Grads:
+      local dFunc = autograd(func)
+
+      -- Compute func and grads:
+      local pred = func({W=W, x=x})
+      local grads = dFunc({W=W, x=x})
+
+      -- Tests:
+      tester:asserteq(type(pred), 'number', 'incorrect prediction')
+      tester:asserteq(pred, 800, 'incorrect prediction')
+      tester:asserteq(grads.W:dim(), 2, 'incorrect dims for gradients')
+      tester:asserteq(grads.W:size(1), 32, 'incorrect dims for gradients')
+      tester:asserteq(grads.W:size(2), 100, 'incorrect dims for gradients')
+      tester:asserteq(grads.x:dim(), 1, 'incorrect dims for gradients')
+      tester:asserteq(grads.x:size(1),100, 'incorrect dims for gradients')
+   end,
+
    DotNonLinear = function()
       -- Parameters:
       local W = torch.Tensor(32,100):fill(.5)
@@ -25,6 +52,7 @@ local tests = {
 
       -- Tests:
       tester:asserteq(type(pred), 'number', 'incorrect prediction')
+      tester:asserteq(pred, 32, 'incorrect prediction')
       tester:asserteq(grads.W:dim(), 2, 'incorrect dims for gradients')
       tester:asserteq(grads.W:size(1), 32, 'incorrect dims for gradients')
       tester:asserteq(grads.W:size(2), 100, 'incorrect dims for gradients')
