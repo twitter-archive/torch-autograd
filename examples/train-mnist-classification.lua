b944fcb6626b0515bcc034f6b901605ddb9072ac
Clement Farabet <cfarabet@twitter.com>
2015-10-04 21:13:37 -0400

    Cleaned up mnist example (classification).

--- examples/train-mnist-classification.lua
+++ examples/train-mnist-classification.lua
@@ -5,9 +5,8 @@ local optim = require 'optim'
 
 -- Helpers:
 local function logMultiNomialLoss(out, target) return -torch.sum(torch.cmul(out,target)) end
-local function logsumexp(array) return torch.log(torch.sum(torch.exp(array))) end
-local function logsoftmax(array) return array - logsumexp(array) end
-local function uniform(min, max, h, w) return torch.mul(torch.FloatTensor():rand(h,w), (max-min)) + min end
+local function logSumExp(array) return torch.log(torch.sum(torch.exp(array))) end
+local function logSoftMax(array) return array - logSumExp(array) end
 
 -- Load in MNIST
 local trainData, testData, classes = require('./setup-data.lua')()
@@ -17,12 +16,12 @@ local inputSize = trainData.x[1]:nElement()
 local confusionMatrix = optim.ConfusionMatrix(classes)
 
 -- Define our neural net
-function neuralNet(params, input, target, return_prediction)
-   local W2 = torch.tanh(input * params.W[1] + params.B[1])
-   local W3 = torch.tanh(W2 * params.W[2] + params.B[2])
-   local W4 = W3 * params.W[3] + params.B[3]
-   local out = logsoftmax(W4)
-   if return_prediction then
+function neuralNet(params, input, target, predictionOnly)
+   local h1 = torch.tanh(input * params.W[1] + params.B[1])
+   local h2 = torch.tanh(h1 * params.W[2] + params.B[2])
+   local h3 = h2 * params.W[3] + params.B[3]
+   local out = logSoftMax(h3)
+   if predictionOnly then
       return out
    else
       return logMultiNomialLoss(out, target)
@@ -30,14 +29,13 @@ function neuralNet(params, input, target, return_prediction)
 end
 
 -- Define our parameters
--- [-1/sqrt(#output), 1/sqrt(#output)]
 torch.manualSeed(0)
-W1 = uniform(-1/math.sqrt(50),1/math.sqrt(50), inputSize, 50)
-B1 = torch.FloatTensor(50):fill(0)
-W2 = uniform(-1/math.sqrt(50),1/math.sqrt(50), 50, 50)
-B2 = torch.FloatTensor(50):fill(0)
-W3 = uniform(-1/math.sqrt(10),1/math.sqrt(10), 50, #classes)
-B3 = torch.FloatTensor(#classes):fill(0)
+local W1 = torch.FloatTensor(inputSize,50):uniform(-1/math.sqrt(50),1/math.sqrt(50))
+local B1 = torch.FloatTensor(50):fill(0)
+local W2 = torch.FloatTensor(50,50):uniform(-1/math.sqrt(50),1/math.sqrt(50))
+local B2 = torch.FloatTensor(50):fill(0)
+local W3 = torch.FloatTensor(50,#classes):uniform(-1/math.sqrt(#classes),1/math.sqrt(#classes))
+local B3 = torch.FloatTensor(#classes):fill(0)
 
 -- Trainable parameters:
 local params = {
@@ -46,12 +44,14 @@ local params = {
 }
 
 -- Train a neural network
-for epoch=1,100 do
+for epoch = 1,100 do
    print('Training Epoch #'..epoch)
-   for i=1,trainData.size do
+   for i = 1,trainData.size do
+      -- Next sample:
       local x = trainData.x[i]:view(1,inputSize)
-      local y = torch.view(trainData.y[i], 1, 10):clone()
+      local y = torch.view(trainData.y[i], 1, 10)
 
+      -- Grads:
       local dneuralNet = grad(neuralNet)
       grads = dneuralNet(params,x,y)
       prediction = neuralNet(params,x,y,true)
@@ -65,8 +65,6 @@ for epoch=1,100 do
       -- Log performance:
       confusionMatrix:add(prediction, y)
       if i % 1000 == 0 then
-         print(params.B[1][1])
-         print(torch.sum(grads.W[1]))
          print(confusionMatrix)
          confusionMatrix:zero()
       end
