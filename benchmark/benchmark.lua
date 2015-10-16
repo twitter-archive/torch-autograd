fccb9819f1c4046981437e63a1a8cf1db6145566
Clement Farabet <cfarabet@twitter.com>
2015-10-16 19:24:58 -0400

    Batched benchmark.

--- benchmark/benchmark.lua
+++ benchmark/benchmark.lua
@@ -2,6 +2,7 @@
 local d = require 'autograd'
 local nn = require 'nn'
 local c = require 'trepl.colorize'
+local getValue = require 'autograd.node'.getValue
 
 -- Test 1: logistic regression
 local tests = {
@@ -202,6 +203,60 @@ local tests = {
          for i = 1,x:size(1) do
             local grads = f(params, x[i], yOneHot[i])
          end
+         tag = sys.toc()
+      end
+
+      return tnn, tag
+   end,
+
+   mlpBatched = function()
+      local tnn, tag
+      local x = torch.FloatTensor(128,100):normal()
+      local y = torch.FloatTensor(128):random(1,10)
+      local yOneHot = d.util.oneHot(y)
+
+      do
+         local model = nn.Sequential()
+         model:add(nn.Linear(100,1000))
+         model:add(nn.Tanh())
+         model:add(nn.Linear(1000,10))
+         model:add(nn.LogSoftMax())
+         model:float()
+         local lossf = nn.ClassNLLCriterion()
+         lossf:float()
+
+         -- force allocs
+         local yhat = model:forward(x)
+         local loss = lossf:forward(yhat, y)
+         local dloss_dyhat = lossf:backward(yhat, y)
+         model:backward(x, dloss_dyhat)
+
+         sys.tic()
+         model:zeroGradParameters()
+         local yhat = model:forward(x)
+         local loss = lossf:forward(yhat, y)
+         local dloss_dyhat = lossf:backward(yhat, y)
+         model:backward(x, dloss_dyhat)
+         tnn = sys.toc()
+      end
+
+      do
+         local f = function(params, x, y)
+            local N = getValue(x):size(1)
+            local h1 = torch.tanh( x * params.W1 + torch.expand(params.b1, N,1000) )
+            local h2 = h1 * params.W2 + torch.expand(params.b2, N,10)
+            local loss, yhat = d.loss.crossEntropy(h2, y)
+            return loss
+         end
+         local params = {
+            W1 = torch.FloatTensor(1000, 100):normal(.01):t(),
+            b1 = torch.FloatTensor(1, 1000):zero(),
+            W2 = torch.FloatTensor(10, 1000):normal(.01):t(),
+            b2 = torch.FloatTensor(1, 10):zero(),
+         }
+
+         sys.tic()
+         local grads = d(f)(params, x, yOneHot)
          tag = sys.toc()
       end
 
