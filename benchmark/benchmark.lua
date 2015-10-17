fc40e3388c6abf4d60f4686573f34d42f7ab9938
Clement Farabet <cfarabet@twitter.com>
2015-10-17 17:42:28 -0400

    one more benchmark

--- benchmark/benchmark.lua
+++ benchmark/benchmark.lua
@@ -228,6 +228,72 @@ local tests = {
       return tnn, tag
    end,
 
+   mlpHybridNoOneHot = function()
+      local tnn, tag
+      local x = tensor(1000,100):normal()
+      local y = tensor(1000):uniform(1.5,10.5):floor()
+
+      do
+         local model = nn.Sequential()
+         model:add(nn.Linear(100,1000))
+         model:add(nn.Tanh())
+         model:add(nn.Linear(1000,10))
+         model:add(nn.LogSoftMax())
+         model:type(ttype)
+         local lossf = nn.ClassNLLCriterion()
+         lossf:type(ttype)
+
+         -- force allocs
+         model:zeroGradParameters()
+         local yhat = model:forward(x[1])
+         local loss = lossf:forward(yhat, y[1])
+         local dloss_dyhat = lossf:backward(yhat, y[1])
+         model:backward(x[1], dloss_dyhat)
+
+         tic()
+         for i = 1,x:size(1) do
+            model:zeroGradParameters()
+            local yhat = model:forward(x[i])
+            local loss = lossf:forward(yhat, y[i])
+            local dloss_dyhat = lossf:backward(yhat, y[i])
+            model:backward(x[i], dloss_dyhat)
+         end
+         tnn = toc()
+      end
+
+      do
+         local lin1 = d.nn.Linear(100,1000)
+         local tanh = d.nn.Tanh()
+         local lin2 = d.nn.Linear(1000,10)
+         local lsm = d.nn.LogSoftMax()
+
+         local f = function(params, x, y)
+            local h1 = tanh( lin1(x, params.W1, params.b1) )
+            local h2 = lin2(h1, params.W2, params.b2)
+            local yhat = lsm(h2)
+            local loss = - torch.sum( torch.narrow(yhat, 1, y, 1) )
+            return loss
+         end
+         local params = {
+            W1 = tensor(1000, 100):normal(.01),
+            b1 = tensor(1000):zero(),
+            W2 = tensor(10, 1000):normal(.01),
+            b2 = tensor(10):zero(),
+         }
+
+         -- force allocs
+         local grads = d(f)(params, x[1], y[1])
+
+         tic()
+         for i = 1,x:size(1) do
+            local grads = d(f)(params, x[i], y[i])
+         end
+         tag = toc()
+      end
+
+      return tnn, tag
+   end,
+
    mlpForward = function()
       local tnn, tag
       local x = tensor(1000,100):normal()
