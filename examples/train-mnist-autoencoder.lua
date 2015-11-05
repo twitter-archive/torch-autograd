96a92e39534c54874e772b56dd98c15f690c2730
Kevin Swersky <kswersky@twitter.com>
2015-11-05 14:51:17 -0500

    Committing grad check code temporarily.

--- examples/train-mnist-autoencoder.lua
+++ examples/train-mnist-autoencoder.lua
@@ -17,32 +17,34 @@ local predict,f,params
 -- Define our neural net
 function predict(params, input)
    -- Encoder
-   local h1 = util.sigmoid(input * params.W[1] + params.B[1]:expand(input:size(1), params.B[1]:size(2)))
-   local h2 = util.sigmoid(h1 * params.W[2] + params.B[2]:expand(input:size(1), params.B[2]:size(2)))
-   local h3 = util.sigmoid(h2 * params.W[3] + params.B[3]:expand(input:size(1), params.B[3]:size(2)))
-   
+   local h1 = util.sigmoid(input * params.W[1] + torch.expand(params.B[1], input:size(1), params.B[1]:size(2)))
+   local h2 = util.sigmoid(h1 * params.W[2] + torch.expand(params.B[2], input:size(1), params.B[2]:size(2)))
+   local h3 = util.sigmoid(h2 * params.W[3] + torch.expand(params.B[3], input:size(1), params.B[3]:size(2)))
+
    -- Decoder
-   local h4 = util.sigmoid(h3 * params.W[3]:t() + params.B[4]:expand(input:size(1), params.B[4]:size(2)))
-   local h5 = util.sigmoid(h2 * params.W[2]:t() + params.B[5]:expand(input:size(1), params.B[5]:size(2)))
-   local out = util.sigmoid(h1 * params.W[1]:t() + params.B[6]:expand(input:size(1), params.B[6]:size(2)))
+   local h4 = util.sigmoid(h3 * torch.t(params.W[3]) + torch.expand(params.B[4], input:size(1), params.B[4]:size(2)))
+   local h5 = util.sigmoid(h4 * torch.t(params.W[2]) + torch.expand(params.B[5], input:size(1), params.B[5]:size(2)))
+   local out = util.sigmoid(h5 * torch.t(params.W[1]) + torch.expand(params.B[6], input:size(1), params.B[6]:size(2)))
 
    return out
 end
 
 -- Define our training loss
-function f(params, input, target, l2Lambda)
+function f(params, input, l2Lambda)
    -- Reconstruction loss
    local prediction = predict(params, input)
    local loss = util.logBCELoss(prediction, input)
-   print(prediction)
-   print(1-prediction)
+
    -- L2 penalty on the weights
-   -- for i=1,#params.W do
-   --    loss = loss + l2Lambda * torch.sum(torch.pow(params.W[i],2))
-   -- end
+   for i=1,#params.W do
+      loss = loss + l2Lambda * torch.sum(torch.pow(params.W[i],2))
+   end
 
    return loss, prediction
 end
+
+-- Get the gradients closure magically:
+local df = grad(f)
 
 sizes = {}
 sizes['input'] = 5
@@ -53,15 +55,15 @@ sizes['h3'] = 10
 -- Define our parameters
 -- [-1/sqrt(#output), 1/sqrt(#output)]
 torch.manualSeed(0)
-local W1 = torch.FloatTensor(sizes['input'],sizes['h1']):uniform(-1/math.sqrt(sizes['h1']),1/math.sqrt(sizes['h1']))
-local W2 = torch.FloatTensor(sizes['h1'],sizes['h2']):uniform(-1/math.sqrt(sizes['h2']),1/math.sqrt(sizes['h2']))
-local W3 = torch.FloatTensor(sizes['h2'],sizes['h3']):uniform(-1/math.sqrt(sizes['h3']),1/math.sqrt(sizes['h3']))
-local B1 = torch.FloatTensor(1, sizes['h1']):fill(0)
-local B2 = torch.FloatTensor(1, sizes['h2']):fill(0)
-local B3 = torch.FloatTensor(1, sizes['h3']):fill(0)
-local B4 = torch.FloatTensor(1, sizes['h2']):fill(0)
-local B5 = torch.FloatTensor(1, sizes['h1']):fill(0)
-local B6 = torch.FloatTensor(1, sizes['input']):fill(0)
+local W1 = torch.DoubleTensor(sizes['input'],sizes['h1']):uniform(-1/math.sqrt(sizes['h1']),1/math.sqrt(sizes['h1']))
+local W2 = torch.DoubleTensor(sizes['h1'],sizes['h2']):uniform(-1/math.sqrt(sizes['h2']),1/math.sqrt(sizes['h2']))
+local W3 = torch.DoubleTensor(sizes['h2'],sizes['h3']):uniform(-1/math.sqrt(sizes['h3']),1/math.sqrt(sizes['h3']))
+local B1 = 0.1 * torch.randn(1, sizes['h1'])
+local B2 = 0.1 * torch.randn(1, sizes['h2'])
+local B3 = 0.1 * torch.randn(1, sizes['h3'])
+local B4 = 0.1 * torch.randn(1, sizes['h2'])
+local B5 = 0.1 * torch.randn(1, sizes['h1'])
+local B6 = 0.1 * torch.randn(1, sizes['input'])
 
 -- Trainable parameters:
 params = {
@@ -69,44 +71,31 @@ params = {
    B = {B1, B2, B3, B4, B5, B6},
 }
 
-x = torch.randn(3, 5):float()
-f(params, x, 2.1)
+x = torch.rand(3, 5)
 
--- -- Define our network
--- function predict(params, input, target)
---    local h1 = pool1(acts1(conv1(reshape(input), params.W[1], params.B[1])))
---    local h2 = pool2(acts2(conv2(h1, params.W[2], params.B[2])))
---    local h3 = linear(flatten(h2), params.W[3], params.B[3])
---    local out = util.logSoftMax(h3)
---    return out
--- end
+print(f(params, x, 0.0))
 
--- -- Define our loss function
--- function f(params, input, target)
---    local prediction = predict(params, input, target)
---    local loss = util.logMultinomialLoss(prediction, target)
---    return loss, prediction
--- end
+-- Check gradients
+-- Computed grad grad
+g = df(params, x, 0.0)
 
-
--- -- Define our parameters
--- -- [-1/sqrt(#output), 1/sqrt(#output)]
--- torch.manualSeed(0)
--- local W1 = torch.FloatTensor(16,1*5*5):uniform(-1/math.sqrt(16),1/math.sqrt(16))
--- local B1 = torch.FloatTensor(16):fill(0)
--- local W2 = torch.FloatTensor(16,16*5*5):uniform(-1/math.sqrt(16),1/math.sqrt(16))
--- local B2 = torch.FloatTensor(16):fill(0)
--- local W3 = torch.FloatTensor(#classes,16*5*5):uniform(-1/math.sqrt(#classes),1/math.sqrt(#classes))
--- local B3 = torch.FloatTensor(#classes):fill(0)
-
--- -- Trainable parameters:
--- params = {
---    W = {W1, W2, W3},
---    B = {B1, B2, B3},
--- }
-
--- -- Get the gradients closure magically:
--- local df = grad(f)
+-- Finite differences check
+for i=1,#params.B do
+   var = params.B[i]:view(-1)
+   g_true = g.B[i]:view(-1)
+   g_est = var:clone()
+   g_est:zero()
+   print(i)
+   for j=1,var:size(1) do
+      var[j] = var[j] + 1e-5
+      f1 = f(params, x, 0.0)
+      var[j] = var[j] - 2e-5
+      f2 = f(params, x, 0.0)
+      var[j] = var[j] + 1e-5
+      g_est[j] = (f1 - f2) / 2e-5
+      print (g_est[j] - g_true[j])
+   end
+end
 
 -- -- Train a neural network
 -- for epoch = 1,100 do
