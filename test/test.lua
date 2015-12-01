c2195ec44f65d6ace75d81d2a40255c587e81dc5
Alex Wiltschko <alex.bw@gmail.com>
2015-12-01 15:02:24 -0500

    Added MLP test

--- test/test.lua
+++ test/test.lua
@@ -195,6 +195,7 @@ local tests = {
       tester:assert(gradcheck(NarrowFn1D, {W=W,x=x1}), "Incorrect gradient")
       tester:assert(gradcheck(NarrowFn2D, {W=W,x=x2}), "Incorrect gradient")
    end,
+   
    Clamp = function()
       local W = torch.Tensor(5,25):normal()
       local clampFn = function(inputs)
@@ -203,6 +204,7 @@ local tests = {
       tester:assert(clampFn({W=W})>0, "Basic sanity check failed")
       tester:assert(gradcheck(clampFn,{W=W}), "Incorrect gradient")
    end,
+   
    Clone = function()
       local x = torch.Tensor(2,25):normal()
 
@@ -657,6 +659,58 @@ local tests = {
       tester:asserteq(grads.W:size(2), 100, 'incorrect dims for gradients')
       tester:asserteq(grads.x:dim(), 1, 'incorrect dims for gradients')
       tester:asserteq(grads.x:size(1),100, 'incorrect dims for gradients')
+   end,
+
+   GradCheck_MLP = function()
+      local inputSize = 1024
+      local classes = {0,1,2,3,4,5,6,7,8,9}
+      -- What model to train:
+      local predict,f,params
+
+      -- Define our neural net
+      function predict(params, input, target)
+         local h1 = torch.tanh(input * params.W[1] + params.B[1])
+         local h2 = torch.tanh(h1 * params.W[2] + params.B[2])
+         local h3 = h2 * params.W[3] + params.B[3]
+         local out = autograd.util.logSoftMax(h3)
+         return out
+      end
+
+      -- Define our training loss
+      function f(params, input, target)
+         local prediction = predict(params, input, target)
+         local loss = autograd.loss.logMultinomialLoss(prediction, target)
+         return loss, prediction
+      end
+
+      -- Define our parameters
+      -- [-1/sqrt(#output), 1/sqrt(#output)]
+      torch.manualSeed(0)
+      local W1 = torch.FloatTensor(inputSize,50):uniform(-1/math.sqrt(50),1/math.sqrt(50))
+      local B1 = torch.FloatTensor(50):fill(0)
+      local W2 = torch.FloatTensor(50,50):uniform(-1/math.sqrt(50),1/math.sqrt(50))
+      local B2 = torch.FloatTensor(50):fill(0)
+      local W3 = torch.FloatTensor(50,#classes):uniform(-1/math.sqrt(#classes),1/math.sqrt(#classes))
+      local B3 = torch.FloatTensor(#classes):fill(0)
+
+      -- Trainable parameters:
+      params = {
+         W = {W1, W2, W3},
+         B = {B1, B2, B3},
+      }
+
+      input = torch.randn(1,1024):float()
+      target = torch.zeros(1,10):float()
+      target[1][3] = 1
+
+      p = {W=params.W,B=params.B, input=input, target=target}
+
+      function testme(params)
+         return f({W=params.W,B=params.B}, params.input, params.target)
+      end
+
+      tester:assert(gradcheck(testme, p), "Incorrect Gradient")
+
    end,
 
    NNFunc_Basic = function()
